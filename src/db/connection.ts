ba3209842363571da23626dd275a7123b47c3b5f
Icaro Peretti <icaroperetti50@gmail.com>
2025-01-01 01:11:47 -0300

    refactor: format with eslint

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,8 @@
-import postgres   from "postgres"
-import {drizzle} from "drizzle-orm/postgres-js"
-import { env } from "../env"
-import * as schema from "./schema"
+import postgres from 'postgres'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import { env } from '../env'
+import * as schema from './schema'
 
+const connection = postgres(env.DATABASE_URL)
 
-const connection = postgres(env.DATABASE_URL,)
-
-export const db = drizzle(connection, {schema})
+export const db = drizzle(connection, { schema })
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,9 @@
-import {Elysia} from 'elysia'
+import { Elysia } from 'elysia'
 
-const app = new Elysia()
-.get('/', () => {return 'Hello World'})
-
-app.listen(3333, () =>{
-  console.log('HTTP server running')
+const app = new Elysia().get('/', () => {
+  return 'Hello World'
 })
 
+app.listen(3333, () => {
+  console.log('HTTP server running')
+})
