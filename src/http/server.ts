488981df1104f3544dd3ac5f869bfbe153c374dd
Icaro Peretti <icaroperetti50@gmail.com>
2025-01-01 21:55:43 -0300

    todo: fix setCookie error

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,12 @@ import { Elysia } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+import { authenticateFromLink } from './routes/authenticate-from-link'
+
+const app = new Elysia()
+  .use(registerRestaurant)
+  .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
