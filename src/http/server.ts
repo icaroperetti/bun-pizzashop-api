a782c45b2e79983ef19a68f7b8fae62c602639ff
Icaro Peretti <icaroperetti50@gmail.com>
2025-01-10 00:06:46 -0300

    feat: route to get order detail with joins

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,6 +6,7 @@ import { authenticateFromLink } from './routes/authenticate-from-link'
 import { signOut } from './routes/sign-out'
 import { getProfile } from './routes/get-profile'
 import { getManegedRestaurant } from './routes/get-managed-restaurant'
+import { getOrderDetails } from './routes/get-order-details'
 
 const app = new Elysia()
   .use(registerRestaurant)
@@ -14,6 +15,7 @@ const app = new Elysia()
   .use(signOut)
   .use(getProfile)
   .use(getManegedRestaurant)
+  .use(getOrderDetails)
   .onError(({ error, code, set }) => {
     switch (code) {
       case 'VALIDATION':
